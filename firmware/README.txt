460f90b8010e6dc2eec416dfd52e3263430eeb8b
shhx <luisgomezlo.lg@gmail.com>
2025-02-16 03:08:02 +0100

    Added code generated by libero

---
+++ firmware/README.txt
@@ -0,0 +1,6 @@
+Export Firmware README
+
+Microchip Technology Inc. - Microchip Libero Software Release v2024.1 (Version 2024.1.0.3)
+
+Date    :    Sat Feb 15 20:44:04 2025
+Project :    C:\Users\luisg\Documents\projects\smartfusion\smartfusion_blink\smartfusion_blink
---
+++ firmware/system_hw_platform.h
@@ -0,0 +1,16 @@
+#ifndef system_HW_PLATFORM_H_
+#define system_HW_PLATFORM_H_
+/*****************************************************************************
+*
+*Created by Microsemi SmartDesign  Sat Feb 15 20:44:04 2025
+*
+*Memory map specification for peripherals in system
+*/
+
+/*-----------------------------------------------------------------------------
+* CM3 subsystem memory map
+* Initiator(s) for this subsystem: CM3 
+*---------------------------------------------------------------------------*/
+
+
+#endif /* system_HW_PLATFORM_H_*/
